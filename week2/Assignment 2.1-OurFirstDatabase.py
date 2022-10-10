1e8d6cf85f3e963fc214e37f1f345efe285816d5
AlexCXK <changing199112@gmail.com>
2022-10-11 00:30:56 +0800

    upload assignment

---
+++ week2/Assignment 2.1-OurFirstDatabase.py
@@ -0,0 +1,20 @@
+"""
+To get credit for this assignment, perform the instructions below and enter the code you get here:
+(Hint: starts with 467)
+Instructions
+create a SQLITE database or use an existing database and create a table in the database called "Ages":
+CREATE TABLE Ages (
+  name VARCHAR(128),
+  age INTEGER
+)
+Then make sure the table is empty by deleting any rows that you previously inserted, and insert these rows and only these rows with the following commands:
+DELETE FROM Ages;
+INSERT INTO Ages (name, age) VALUES ('Mara', 28);
+INSERT INTO Ages (name, age) VALUES ('Otto', 33);
+INSERT INTO Ages (name, age) VALUES ('Fyn', 31);
+INSERT INTO Ages (name, age) VALUES ('Neshawn', 17);
+Once the inserts are done, run the following SQL command:
+SELECT hex(name || age) AS X FROM Ages ORDER BY X
+Find the first row in the resulting record set and enter the long string that looks like 53656C696E613333.
+Answer ==> The first row in the resulting record set : 46796E3331
+"""
